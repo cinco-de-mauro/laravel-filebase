9b6a1debdcd3a12ab4173983647332c97412fc6b
Mauro <mauro@lbm.co>
2020-09-15 16:44:59 -0400

    composer start and service provider start

---
+++ src/MongodbServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CincoDeMauro\LaravelFilebase;
+
+class LaravelFilebaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     */
+    public function register()
+    {
+
+    }
+}
